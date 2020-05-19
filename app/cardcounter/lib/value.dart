f97f9442e0464bdc4e2b803fdf693bdfda7c6d5f
Asti7 <astitvagautam@gmail.com>
2020-05-20 02:28:49 +1000

    game logic model added

---
+++ app/cardcounter/lib/value.dart
@@ -0,0 +1,25 @@
+class Values {
+  List cardValues = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'A',
+    'J',
+    'K',
+    'Q'
+  ];
+
+  List negativeOneValues = ['2', '3', '4', '5', '6'];
+
+  List zeroValues = ['7', '8', '9'];
+
+  List positiveOneValues = ['10', 'A', 'J', 'K', 'Q'];
+
+  List cardTypes = ['C', 'D', 'H', 'S'];
+}
